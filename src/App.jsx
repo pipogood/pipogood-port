1de4285ccae8dd869000af938fde1d013eef88c9
pipogood <82525600+pipogood@users.noreply.github.com>
2025-06-07 14:43:57 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -18,12 +18,12 @@ const App = () => {
 
     return (
       <BrowserRouter>
-        {/* <div className="relative z-0 bg-black-100 bg-cover bg-no-repeat bg-center"> */}
+        <div className="relative z-0 bg-black-100 bg-cover bg-no-repeat bg-center">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/page2" element={<Page2 />} />
           </Routes>
-        {/* </div> */}
+        </div>
       </BrowserRouter>
     );
 }
